9c32f986c268e1d79370035f5d63335ffbf9e288
Alex <Alex@BB8>
2018-02-14 10:47:12 +0300

    Add reasource

---
+++ Youtube-dl-Gui/Views/IView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Youtube_dl_Gui
+{
+    public interface IView
+    {
+        void Show();
+        void Close();
+    }
+}
